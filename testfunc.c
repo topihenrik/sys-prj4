3adb6a2de9d546666981d03efbd80f3015d6c98e
Topi Ilmoni <topi.ilmoni@gmail.com>
2022-04-25 00:30:04 +0300

    improved the testfunc.c test file

--- testfunc.c
+++ testfunc.c
@@ -1,9 +1,21 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
 
 int main(int argc, char *argv[]) {
-    int number = readcount();
+    int fd, number, size;
+    char buffer[128];
+
+    number = readcount();
     printf(1, "readcount is currently: %d\n", number);
+
+    fd = open("readtest.txt", O_RDONLY);
+    size = sizeof(buffer);
+    read(fd, &buffer, size);
+    number = readcount();
+    printf(1, "reacount after calling read(): %d\n", number);
+    close(fd);
+
     exit();
 }
